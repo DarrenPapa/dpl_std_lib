1fddeb26cc4d002c4b622ca23d6902825af6d675
Darren Papa <darrenchasepapa@gmai.com>
2024-10-22 21:06:01 +0800

    QUICK PATCH again

--- case_op.py
+++ case_op.py
@@ -4,30 +4,27 @@ if __name__ != "__dpl__":
 temp = {
 }
 
-# helper function
-def fn(body):
-    return add_func(frame=temp)(body)
-
-@fn
+@add_method()
 def isUpper(_, __, string):
     return string.isupper(),
 
-@fn
+@add_method()
 def isLower(_, __, string):
     return string.islower(),
 
-@fn
+@add_method()
 def toUpper(_, __, string):
     return string.upper(),
 
-@fn
+@add_method()
 def toLower(_, __, string):
     return string.lower()
 
-@fn
+@add_method()
 def titleCase(_, __, string):
     return string.title(),
 
+@add_method()
 @fn
 def capitalize(_, __, string):
     return string.capitalize(),
